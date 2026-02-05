6fecd0252c80bbd1acc02b30f5b5a7fc09ffbc74
Saviru Sobash Jayasingha Gunawardana <as20240540@sci.sjp.ac.lk>
2026-02-05 23:17:04 +0530

    impelement user login and registration

--- frontend/common/header.php
+++ frontend/common/header.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/user-flood-relief-styles.css">
     <link rel="stylesheet" href="../css/view-relief-request.css">
-
+    <link rel="stylesheet" href="../css/login-register-style.css">
     
 </head>
 <body>
