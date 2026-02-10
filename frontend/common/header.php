7232e80234f164b4539df8f9de612dfb63a7c760
AvPiyadasa75 <as20240574@sci.sjp.ac.lk>
2026-02-11 00:32:08 +0530

    update user relief request form

--- frontend/common/header.php
+++ frontend/common/header.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AquaRelief</title>
     <link rel="stylesheet" href="../css/user-flood-relief-styles.css">
+    <link rel="stylesheet" href="../css/admin.css">
+    
     
 </head>
 <body>
